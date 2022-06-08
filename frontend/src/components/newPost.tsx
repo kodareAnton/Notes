332b02f84a380c1ca6158f84d515c5005d33be51
Jonas <jonasericsson90@hotmail.com>
2022-06-08 21:44:18 +0200

    post

--- frontend/src/components/newPost.tsx
+++ frontend/src/components/newPost.tsx
@@ -1,16 +1,41 @@
 import { Editor } from "@tinymce/tinymce-react";
+import axios from "axios";
 import { useRef } from "react";
+import { IPost } from "../models/Ipost";
 
 export function NewPost() {
-  const editorRef = useRef(null);
+  //   const editorRef = useRef(null);
   const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
+    const testObj = {
+      title: "Jonas",
+      description: "lalalala",
+      date: 20220101,
+    };
+    console.log(testObj);
+
+    const res = axios.post<IPost>(
+      "http://localhost:3000/users/add",
+      {
+        title: "Jonas",
+        description: "lalalala",
+        date: "20220101",
+      },
+      {
+        headers: {},
+      }
+    );
+
+    return res.data.headers["Content-Type"]; // text/json
   };
+
+  //   const log = () => {
+  //     if (editorRef.current) {
+  //       console.log(editorRef.current.getContent());
+  //     }
+  //   };
   return (
     <>
-      <Editor
+      {/* <Editor
         disabled={true}
         apiKey="no-api-key"
         onInit={(evt, editor) => (editorRef.current = editor)}
@@ -46,7 +71,7 @@ export function NewPost() {
           content_style:
             "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
         }}
-      />
+      /> */}
       <button onClick={log}> Log editor content</button>
     </>
   );
